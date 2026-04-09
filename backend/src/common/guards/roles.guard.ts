2a4a452c3feda44bf43344be88d6c8d8ee46b022
MULUALEM MEKONIN <mulualemmekonin0@gmail.com>
2026-04-08 23:17:12 -0700

    implement role

---
+++ backend/src/common/guards/roles.guard.ts
@@ -0,0 +1,27 @@
+// src/auth/guards/roles.guard.ts
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../enums/roles.enum';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class DbRolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) return true; // no roles required
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    console.log("the user information is " ,user)
+    if (!user || !user.userId) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+    return requiredRoles.includes(user.role);
+  }
+}
